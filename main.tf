0578f1e751953f00c71b37cfc1cfe7ae09ab2a67
MateSousa <matessousa@outlook.com>
2023-07-23 19:47:33 -0300

    w

--- main.tf
+++ main.tf
@@ -1,5 +1,11 @@
 provider "kubernetes" {
-  config_path = "~/.kube/config"  # Path to your kubeconfig file
+  config_path = "~/.kube/config" # Path to your kubeconfig file
+}
+
+provider "helm" {
+  kubernetes {
+    config_path = "~/.kube/config" # Path to your kubeconfig file
+  }
 }
 
 resource "kubernetes_namespace" "example" {
@@ -9,28 +15,22 @@ resource "kubernetes_namespace" "example" {
 }
 
 
-provider "helm" {
-  kubernetes {
-    config_path = "~/.kube/config"  # Path to your kubeconfig file
+
+resource "helm_release" "argocd" {
+  name       = "argocd"
+  repository = "https://argoproj.github.io/argo-helm"
+  chart      = "argo-cd"
+  namespace  = kubernetes_namespace.example.metadata[0].name
+
+  set {
+    name  = "server.extraArgs[0]"
+    value = "--insecure"
   }
 }
 
-resource "helm_release" "argocd" {
-    name       = "argocd"
-    repository = "https://argoproj.github.io/argo-helm"
-    chart      = "argo-cd"
-    namespace  = kubernetes_namespace.example.metadata[0].name
-    
-    set {
-        name = "server.extraArgs[0]"
-        value = "--insecure"
-    }
-}
-
 resource "helm_release" "crossplane" {
-    name       = "crossplane"
-    repository = "https://charts.crossplane.io/stable"
-    chart      = "crossplane"
-    namespace  = kubernetes_namespace.example.metadata[0].name
+  name       = "crossplane"
+  repository = "https://charts.crossplane.io/stable"
+  chart      = "crossplane"
+  namespace  = kubernetes_namespace.example.metadata[0].name
 }
-
